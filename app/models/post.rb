de272a846966156cee7ebaa3f5257f16dc602c93
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-21 14:14:27 -0500

    add uuid column to post table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+	before_create :create_uuid
+	private
+	def create_uuid
+	    begin
+	      self.uuid = SecureRandom.uuid
+	    end while self.class.exists?(:uuid => uuid)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621161407) do
+ActiveRecord::Schema.define(version: 20170621191000) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
 end
