68241d4e610f3198f2e1fbf153ae599f935cb301
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-21 13:54:40 -0500

    add edit and update actions to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
 	def new
 		@post = Post.new
 	end
+	
 	def create
 		@post = Post.new(post_params)
 		if @post.save
@@ -14,6 +16,20 @@ class PostsController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+		@post = Post.find_by(params[:id])
+	end
+
+	def update
+		@post = Post.find_by(params[:id])
+	    if @post.update_attributes(post_params)
+	      redirect_to root_path
+	    else
+	      render 'index'
+	    end
+	end
+	
 	private 
 	def post_params
 		params.require(:post).permit(:title, :text)
