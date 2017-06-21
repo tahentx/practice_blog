bc7f88e4a803a2d073a49e6afea453e74647b8b0
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-21 14:03:57 -0500

    implement delete and new post actions to blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,12 @@ class PostsController < ApplicationController
 	    end
 	end
 	
+	def destroy
+		@post = Post.find_by(params[:id])
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private 
 	def post_params
 		params.require(:post).permit(:title, :text)
